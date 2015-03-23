8e1b20d85eb2e35169dd8f710b2d66c01149766b
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-23 02:38:06 +0100

    MXA.Framework.Graphics API classes was created.

---
+++ src/Framework.Graphics/DisplayModeCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insma.Mxa.Framework.Graphics {
+	public class DisplayModeCollection : IEnumerable<DisplayMode>, IEnumerable {
+	}
+}
---
+++ src/Framework.Graphics/ModelEffectCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insma.Mxa.Framework.Graphics {
+	public sealed class ModelEffectCollection : ReadOnlyCollection<Effect> {
+	}
+}
