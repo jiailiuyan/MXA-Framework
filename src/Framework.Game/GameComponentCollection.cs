d8a2ca31206f39393563ccb5d1b3fdb1a45e7ccf
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-23 01:14:47 +0100

    MXA.Framework.Game API classes was created.

---
+++ src/Framework.Game/GameComponentCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insma.Mxa.Framework.Game {
+	public sealed class GameComponentCollection : Collection<IGameComponent> {
+	}
+}
---
+++ src/Framework.Game/GamerServices/GamerServicesComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insma.Mxa.Framework.Game;
+
+namespace Insma.Mxa.Framework.GamerServices {
+	public class GamerServicesComponent : GameComponent {
+	}
+}
