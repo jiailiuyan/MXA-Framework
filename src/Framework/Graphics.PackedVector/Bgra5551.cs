3a5f5ff0644671119e5d4afd1e5598f1571c9248
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 20:46:05 +0100

    [Framework] Bgra5551 API was initialized.

--- src/Framework/Graphics.PackedVector/Bgra5551.cs
+++ src/Framework/Graphics.PackedVector/Bgra5551.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics.PackedVector {
 	public struct Bgra5551 : IPackedVector<ushort>, IPackedVector, IEquatable<Bgra5551> {
+		public Bgra5551(float x, float y, float z, float w) { throw new NotImplementedException( ); }
+		public Bgra5551(Vector4 vector) { throw new NotImplementedException( ); }
+		public bool Equals(Bgra5551 other) { throw new NotImplementedException( ); }
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+		public Vector3 ToVector4( ) { throw new NotImplementedException( ); }
+		public static bool operator !=(Bgra5551 a, Bgra5551 b) { throw new NotImplementedException( ); }
+		public static bool operator ==(Bgra5551 a, Bgra5551 b) { throw new NotImplementedException( ); }
+
+		public ushort PackedValue { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+
+		void IPackedVector.PackFromVector4(Vector4 vector) { throw new NotImplementedException( ); }
+		Vector4 IPackedVector.ToVector4( ) { throw new NotImplementedException( ); }
 	}
 }
