c424a4a8b1997a34255540e4ac24663db051bddb
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 17:23:12 +0100

    [Framework.Graphics] DisplayModeCollection API was initialized.

--- src/Framework.Graphics/DisplayModeCollection.cs
+++ src/Framework.Graphics/DisplayModeCollection.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public class DisplayModeCollection : IEnumerable<DisplayMode>, IEnumerable {
+		public IEnumerator<DisplayMode> GetEnumerator( ) { throw new NotImplementedException( ); }
+		public IEnumerable<DisplayMode> this[SurfaceFormat format] { get { throw new NotImplementedException( ); } }
+		IEnumerator IEnumerable.GetEnumerator( ) { throw new NotImplementedException( ); }
 	}
 }
