113308b7cccf7650fe4b05570137ce6e9cdb95d6
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 19:51:00 +0100

    [Framework] SongCollection API was initialized.

--- src/Framework/Media/SongCollection.cs
+++ src/Framework/Media/SongCollection.cs
@@ -7,5 +7,11 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Media {
 	public sealed class SongCollection : IEnumerable<Song>, IEnumerable, IDisposable {
+		public void Dispose( ) { throw new NotImplementedException( ); }
+		public IEnumerator<Song> GetEnumerator( ) { throw new NotImplementedException( ); }
+		IEnumerator IEnumerable.GetEnumerator( ) { throw new NotImplementedException( ); }
+		public int Count { get { throw new NotImplementedException( ); } }
+		public bool IsDisposed { get { throw new NotImplementedException( ); } }
+		public Song this[int index] { get { throw new NotImplementedException( ); } }
 	}
 }
