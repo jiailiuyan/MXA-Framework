7d52451e113353d747e024157ab6daefb7b69278
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 20:45:28 +0100

    [Framework] Bgra4444 API was initialized.

--- src/Framework/Graphics.PackedVector/Bgra4444.cs
+++ src/Framework/Graphics.PackedVector/Bgra4444.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics.PackedVector {
 	public struct Bgra4444 : IPackedVector<ushort>, IPackedVector, IEquatable<Bgra4444> {
+		public Bgra4444(float x, float y, float z, float w) { throw new NotImplementedException( ); }
+		public Bgra4444(Vector4 vector) { throw new NotImplementedException( ); }
+		public bool Equals(Bgra4444 other) { throw new NotImplementedException( ); }
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+		public Vector3 ToVector4( ) { throw new NotImplementedException( ); }
+		public static bool operator !=(Bgra4444 a, Bgra4444 b) { throw new NotImplementedException( ); }
+		public static bool operator ==(Bgra4444 a, Bgra4444 b) { throw new NotImplementedException( ); }
+
+		public ushort PackedValue { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+
+		void IPackedVector.PackFromVector4(Vector4 vector) { throw new NotImplementedException( ); }
+		Vector4 IPackedVector.ToVector4( ) { throw new NotImplementedException( ); }
 	}
 }
