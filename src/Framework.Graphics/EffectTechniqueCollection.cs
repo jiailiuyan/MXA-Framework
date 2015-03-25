a5a97f05d51db543b0e5a4b39e57e9fd10f22d6f
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 17:33:22 +0100

    [Framework.Graphics] EffectTechniqueCollection API was initialized.

--- src/Framework.Graphics/EffectTechniqueCollection.cs
+++ src/Framework.Graphics/EffectTechniqueCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,10 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public sealed class EffectTechniqueCollection : IEnumerable<EffectTechnique> {
+		public IEnumerator<EffectTechnique> GetEnumerator( ) { throw new NotImplementedException( ); }
+		IEnumerator IEnumerable.GetEnumerator( ) { throw new NotImplementedException( ); }
+		public int Count { get { throw new NotImplementedException( ); } }
+		public EffectTechnique this[int index] { get { throw new NotImplementedException( ); } }
+		public EffectTechnique this[string name] { get { throw new NotImplementedException( ); } }
 	}
 }
