0465331c6b1a78beadc70754ec81155e5eb804b7
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 20:22:25 +0100

    [Framework] Playlist API was initialized.

--- src/Framework/Media/Playlist.cs
+++ src/Framework/Media/Playlist.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Media {
 	public sealed class Playlist : IEquatable<Playlist>, IDisposable {
+		public void Dispose( ) { throw new NotImplementedException( ); }
+		public bool Equals(Playlist other) { throw new NotImplementedException( ); }
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+		public static bool operator !=(Playlist first, Playlist second) { throw new NotImplementedException( ); }
+		public static bool operator ==(Playlist first, Playlist second) { throw new NotImplementedException( ); }
+		public System.TimeSpan Duration { get { throw new NotImplementedException( ); } }
+		public bool IsDisposed { get { throw new NotImplementedException( ); } }
+		public string Name { get { throw new NotImplementedException( ); } }
+		public SongCollection Songs { get { throw new NotImplementedException( ); } }
 	}
 }
