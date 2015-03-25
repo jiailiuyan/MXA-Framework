5d8459311b6a59714c2f3a851bd41b109e9e7121
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 19:37:58 +0100

    [Framework] CurveKeyCollection API was initialized.

--- src/Framework/CurveKeyCollection.cs
+++ src/Framework/CurveKeyCollection.cs
@@ -7,5 +7,19 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework {
 	public class CurveKeyCollection : ICollection<CurveKey>, IEnumerable<CurveKey>, IEnumerable {
+		public CurveKeyCollection( ) { throw new NotImplementedException( ); }
+		public void Add(CurveKey item) { throw new NotImplementedException( ); }
+		public void Clear( ) { throw new NotImplementedException( ); }
+		public CurveKeyCollection Clone( ) { throw new NotImplementedException( ); }
+		public bool Contains(CurveKey item) { throw new NotImplementedException( ); }
+		public void CopyTo(CurveKey[] array, int arrayIndex) { throw new NotImplementedException( ); }
+		public IEnumerator<CurveKey> GetEnumerator( ) { throw new NotImplementedException( ); }
+		IEnumerator IEnumerable.GetEnumerator( ) { throw new NotImplementedException( ); }
+		public int IndexOf(CurveKey item) { throw new NotImplementedException( ); }
+		public bool Remove(CurveKey item) { throw new NotImplementedException( ); }
+		public void RemoveAt(int index) { throw new NotImplementedException( ); }
+		public int Count { get { throw new NotImplementedException( ); } }
+		public bool IsReadOnly { get { throw new NotImplementedException( ); } }
+		public CurveKey this[int index] { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
 	}
 }
