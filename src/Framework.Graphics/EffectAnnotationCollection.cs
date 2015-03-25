ad4848ee586e7beaebc4be1c984ffa98f0157606
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-25 17:30:11 +0100

    [Framework.Graphics] EffectAnnotationCollection API was initialized.

--- src/Framework.Graphics/EffectAnnotationCollection.cs
+++ src/Framework.Graphics/EffectAnnotationCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,10 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public sealed class EffectAnnotationCollection : IEnumerable<EffectAnnotation> {
+		public IEnumerator<EffectAnnotation> GetEnumerator( ) { throw new NotImplementedException( ); }
+		IEnumerator IEnumerable.GetEnumerator( ) { throw new NotImplementedException( ); }
+		public int Count { get { throw new NotImplementedException( ); } }
+		public EffectAnnotation this[int index] { get { throw new NotImplementedException( ); } }
+		public EffectAnnotation this[string name] { get { throw new NotImplementedException( ); } }
 	}
 }
