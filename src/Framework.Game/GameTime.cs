d296793fec26bf152c83f2bd9b009082137434cf
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 16:09:27 +0100

    [Framework.Game] GameTime API was initialized.

--- src/Framework.Game/GameTime.cs
+++ src/Framework.Game/GameTime.cs
@@ -4,7 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Insma.Mxa.Framework.Game {
+namespace Insma.Mxa.Framework {
 	public class GameTime {
+		public GameTime( ) { throw new NotImplementedException( ); }
+		public GameTime(TimeSpan totalGameTime, TimeSpan elapsedGameTime) { throw new NotImplementedException( ); }
+		public GameTime(TimeSpan totalGameTime, TimeSpan elapsedGameTime, bool isRunningSlowly) { throw new NotImplementedException( ); }
+
+		public TimeSpan ElapsedGameTime { get { throw new NotImplementedException( ); } internal set { throw new NotImplementedException( ); } }
+		public bool IsRunningSlowly { get { throw new NotImplementedException( ); } internal set { throw new NotImplementedException( ); } }
+		public TimeSpan TotalGameTime { get { throw new NotImplementedException( ); } internal set { throw new NotImplementedException( ); } }
 	}
 }
