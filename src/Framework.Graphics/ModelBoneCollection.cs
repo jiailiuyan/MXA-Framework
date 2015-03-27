2ca899ed778fe0c164e7cf48cce84c873b841bda
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 14:08:22 +0100

    [Framework.Graphics] ModelBoneCollection API was initialized.

--- src/Framework.Graphics/ModelBoneCollection.cs
+++ src/Framework.Graphics/ModelBoneCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,5 +8,21 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public sealed class ModelBoneCollection : ReadOnlyCollection<ModelBone> {
+		internal ModelBoneCollection(IList<ModelBone> list) : base(list) { throw new NotImplementedException( ); }
+
+		public new ModelBoneCollection.Enumerator GetEnumerator( ) { throw new NotImplementedException( ); }
+		public bool TryGetValue(string boneName, out ModelBone value) { throw new NotImplementedException( ); }
+
+		public ModelBone this[string boneName] { get { throw new NotImplementedException( ); } }
+
+		public struct Enumerator : IEnumerator<ModelBone>, IDisposable, IEnumerator {
+			public void Dispose( ) { throw new NotImplementedException( ); }
+			public bool MoveNext( ) { throw new NotImplementedException( ); }
+
+			public ModelBone Current { get { throw new NotImplementedException( ); } }
+
+			void IEnumerator.Reset( ) { throw new NotImplementedException( ); }
+			object IEnumerator.Current { get { throw new NotImplementedException( ); } }
+		}
 	}
 }
