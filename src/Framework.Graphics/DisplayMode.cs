8c7942095cf741d665403aa6be54142030e605dc
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 01:01:05 +0100

    [Framework.Graphics] DisplayMode API was initialized.

--- src/Framework.Graphics/DisplayMode.cs
+++ src/Framework.Graphics/DisplayMode.cs
@@ -6,5 +6,12 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public class DisplayMode {
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public float AspectRatio { get { throw new NotImplementedException( ); } }
+		public SurfaceFormat Format { get { throw new NotImplementedException( ); } }
+		public int Height { get { throw new NotImplementedException( ); } }
+		public Rectangle TitleSafeArea { get { throw new NotImplementedException( ); } }
+		public int Width { get { throw new NotImplementedException( ); } }
 	}
 }
