937f91276a08ea3d81fac444ed613b0caef2ceec
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 15:40:52 +0100

    [Framework.Game] IUpdateable API was initialized.

--- src/Framework.Game/IUpdateable.cs
+++ src/Framework.Game/IUpdateable.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Insma.Mxa.Framework.Game {
+namespace Insma.Mxa.Framework {
 	public interface IUpdateable {
+		void Update(GameTime gameTime);
+		bool Enabled { get; }
+		int UpdateOrder { get; }
+		event EventHandler<EventArgs> EnabledChanged;
+		event EventHandler<EventArgs> UpdateOrderChanged;
 	}
 }
