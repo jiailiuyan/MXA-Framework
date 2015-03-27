1ae5e579c1326ff0badde24d4c318424352093bc
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 15:39:38 +0100

    [Framework.Game] IDrawable API was initialized.

--- src/Framework.Game/IDrawable.cs
+++ src/Framework.Game/IDrawable.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Insma.Mxa.Framework.Game {
+namespace Insma.Mxa.Framework {
 	public interface IDrawable {
+		void Draw(GameTime gameTime);
+		int DrawOrder { get; }
+		bool Visible { get; }
+		event EventHandler<EventArgs> DrawOrderChanged;
+		event EventHandler<EventArgs> VisibleChanged;
 	}
 }
