60af67f6979331e941f1cd8c11f771043061e675
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 16:11:28 +0100

    [Framework.Game] GamerServicesComponent API was initialized.

--- src/Framework.Game/GamerServices/GamerServicesComponent.cs
+++ src/Framework.Game/GamerServices/GamerServicesComponent.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Insma.Mxa.Framework.Game;
 
 namespace Insma.Mxa.Framework.GamerServices {
 	public class GamerServicesComponent : GameComponent {
+		public GamerServicesComponent(Game game) : base(game) { throw new NotImplementedException( ); }
+
+		public override void Initialize( ) { throw new NotImplementedException( ); }
+		public override void Update(GameTime gameTime) { throw new NotImplementedException( ); }
 	}
 }
