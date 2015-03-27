06742ac080d257842843804d1f0a1d293d8d7e16
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 16:09:14 +0100

    [Framework.Game] LaunchParameters API was initialized.

--- src/Framework.Game/LaunchParameters.cs
+++ src/Framework.Game/LaunchParameters.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Insma.Mxa.Framework.Game {
+namespace Insma.Mxa.Framework {
 	public class LaunchParameters : Dictionary<string, string> {
+		public LaunchParameters( ) { throw new NotImplementedException( ); }
 	}
 }
