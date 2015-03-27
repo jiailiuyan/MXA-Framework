9e2511e62197d0b017195dff48b3cf97fa519853
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-27 16:10:21 +0100

    [Framework.Game] GameServiceContainer API was initialized.

--- src/Framework.Game/GameServiceContainer.cs
+++ src/Framework.Game/GameServiceContainer.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Insma.Mxa.Framework.Game {
+namespace Insma.Mxa.Framework {
 	public class GameServiceContainer : IServiceProvider {
+		public GameServiceContainer( ) { throw new NotImplementedException( ); }
+
+		public void AddService(Type type, object provider) { throw new NotImplementedException( ); }
+		public object GetService(Type type) { throw new NotImplementedException( ); }
+		public void RemoveService(Type type) { throw new NotImplementedException( ); }
 	}
 }
