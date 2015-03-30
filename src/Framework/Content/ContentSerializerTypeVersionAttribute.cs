af717e1339f4906ed801e6f0dbb1dcac16f34395
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:06:58 +0200

    [Framework] ContentSerializerTypeVersionAttribute API was initialized.

--- src/Framework/Content/ContentSerializerTypeVersionAttribute.cs
+++ src/Framework/Content/ContentSerializerTypeVersionAttribute.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 namespace Insma.Mxa.Framework.Content {
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 	public sealed class ContentSerializerTypeVersionAttribute : Attribute {
+		public ContentSerializerTypeVersionAttribute(int typeVersion) { throw new NotImplementedException( ); }
+
+		public int TypeVersion { get { throw new NotImplementedException( ); } }
 	}
 }
