8f65d2bb989e0dae1b3a6ad126c6fb6be47dfd32
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:23:06 +0200

    [Framework] AudioListener API was initialized.

--- src/Framework/Audio/AudioListener.cs
+++ src/Framework/Audio/AudioListener.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Audio {
 	public class AudioListener {
+		public AudioListener( ) { throw new NotImplementedException( ); }
+
+		public Vector3 Forward { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Position { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Up { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Velocity { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
 	}
 }
