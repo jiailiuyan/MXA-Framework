61db8581e12700c8bbdb52c5a82c57293fc29ca6
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:27:48 +0200

    [Framework] GamePadCapabilities API was initialized.

--- src/Framework/Input/GamePadCapabilities.cs
+++ src/Framework/Input/GamePadCapabilities.cs
@@ -6,5 +6,31 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct GamePadCapabilities {
+		public GamePadType GamePadType { get { throw new NotImplementedException( ); } }
+		public bool HasAButton { get { throw new NotImplementedException( ); } }
+		public bool HasBackButton { get { throw new NotImplementedException( ); } }
+		public bool HasBButton { get { throw new NotImplementedException( ); } }
+		public bool HasBigButton { get { throw new NotImplementedException( ); } }
+		public bool HasDPadDownButton { get { throw new NotImplementedException( ); } }
+		public bool HasDPadLeftButton { get { throw new NotImplementedException( ); } }
+		public bool HasDPadRightButton { get { throw new NotImplementedException( ); } }
+		public bool HasDPadUpButton { get { throw new NotImplementedException( ); } }
+		public bool HasLeftShoulderButton { get { throw new NotImplementedException( ); } }
+		public bool HasLeftStickButton { get { throw new NotImplementedException( ); } }
+		public bool HasLeftTrigger { get { throw new NotImplementedException( ); } }
+		public bool HasLeftVibrationMotor { get { throw new NotImplementedException( ); } }
+		public bool HasLeftXThumbStick { get { throw new NotImplementedException( ); } }
+		public bool HasLeftYThumbStick { get { throw new NotImplementedException( ); } }
+		public bool HasRightShoulderButton { get { throw new NotImplementedException( ); } }
+		public bool HasRightStickButton { get { throw new NotImplementedException( ); } }
+		public bool HasRightTrigger { get { throw new NotImplementedException( ); } }
+		public bool HasRightVibrationMotor { get { throw new NotImplementedException( ); } }
+		public bool HasRightXThumbStick { get { throw new NotImplementedException( ); } }
+		public bool HasRightYThumbStick { get { throw new NotImplementedException( ); } }
+		public bool HasStartButton { get { throw new NotImplementedException( ); } }
+		public bool HasVoiceSupport { get { throw new NotImplementedException( ); } }
+		public bool HasXButton { get { throw new NotImplementedException( ); } }
+		public bool HasYButton { get { throw new NotImplementedException( ); } }
+		public bool IsConnected { get { throw new NotImplementedException( ); } }
 	}
 }
