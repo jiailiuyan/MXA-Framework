1f133ca1e16c900f53bc2baac6e6d7633f97dcf6
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:45:08 +0200

    [Framework] VisualizationData API was initialized.

--- src/Framework/Media/VisualizationData.cs
+++ src/Framework/Media/VisualizationData.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Media {
 	public class VisualizationData {
+		public VisualizationData( ) { throw new NotImplementedException( ); }
+
+		public ReadOnlyCollection<float> Frequencies { get { throw new NotImplementedException( ); } }
+		public ReadOnlyCollection<float> Samples { get { throw new NotImplementedException( ); } }
 	}
 }
