2cd83a375bf8c01573b9026a654ef9cc80c380ab
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:00:52 +0200

    [Framework] ContentTypeReader API was initialized.

--- src/Framework/Content/ContentTypeReader.cs
+++ src/Framework/Content/ContentTypeReader.cs
@@ -6,5 +6,12 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Content {
 	public abstract class ContentTypeReader {
+		protected ContentTypeReader(Type targetType) { throw new NotImplementedException( ); }
+		protected internal virtual void Initialize(ContentTypeReaderManager manager) { throw new NotImplementedException( ); }
+		protected internal abstract object Read(ContentReader input, object existingInstance);
+
+		public virtual bool CanDeserializeIntoExistingObject { get { throw new NotImplementedException( ); } }
+		public Type TargetType { get { throw new NotImplementedException( ); } }
+		public virtual int TypeVersion { get { throw new NotImplementedException( ); } }
 	}
 }
