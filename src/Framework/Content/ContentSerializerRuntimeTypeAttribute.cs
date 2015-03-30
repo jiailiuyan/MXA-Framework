83dc921b4987d46a53c97f22e5af841bb565d1e8
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:06:48 +0200

    [Framework] ContentSerializerRuntimeTypeAttribute API was initialized.

--- src/Framework/Content/ContentSerializerRuntimeTypeAttribute.cs
+++ src/Framework/Content/ContentSerializerRuntimeTypeAttribute.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 namespace Insma.Mxa.Framework.Content {
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 	public sealed class ContentSerializerRuntimeTypeAttribute : Attribute {
+		public ContentSerializerRuntimeTypeAttribute(string runtimeType) { throw new NotImplementedException( ); }
+
+		public string RuntimeType { get { throw new NotImplementedException( ); } }
 	}
 }
