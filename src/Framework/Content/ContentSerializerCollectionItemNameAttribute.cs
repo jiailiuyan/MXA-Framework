01ca6f443950ae161d8e785280612e04278501ac
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:06:29 +0200

    [Framework] ContentSerializerCollectionItemNameAttribute API was initialized.

--- src/Framework/Content/ContentSerializerCollectionItemNameAttribute.cs
+++ src/Framework/Content/ContentSerializerCollectionItemNameAttribute.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 namespace Insma.Mxa.Framework.Content {
 	[AttributeUsage(AttributeTargets.Class)]
 	public sealed class ContentSerializerCollectionItemNameAttribute : Attribute {
+		public ContentSerializerCollectionItemNameAttribute(string collectionItemName) { throw new NotImplementedException( ); }
+
+		public string CollectionItemName { get { throw new NotImplementedException( ); } }
 	}
 }
