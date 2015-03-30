1dc6d095da979157d68d012ef941dde1d835427d
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:06:20 +0200

    [Framework] ContentSerializerAttribute API was initialized.

--- src/Framework/Content/ContentSerializerAttribute.cs
+++ src/Framework/Content/ContentSerializerAttribute.cs
@@ -7,5 +7,16 @@ using System.Threading.Tasks;
 namespace Insma.Mxa.Framework.Content {
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 	public sealed class ContentSerializerAttribute : Attribute {
+		public ContentSerializerAttribute( ) { throw new NotImplementedException( ); }
+
+		public ContentSerializerAttribute Clone( ) { throw new NotImplementedException( ); }
+
+		public bool AllowNull { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public string CollectionItemName { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public string ElementName { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public bool FlattenContent { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public bool HasCollectionItemName { get { throw new NotImplementedException( ); } }
+		public bool Optional { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public bool SharedResource { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
 	}
 }
