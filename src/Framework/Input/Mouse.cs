6ad5cc2fb41cc090f2aba85e76d05a3db02f2411
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:44:29 +0200

    [Framework] Mouse API was initialized.

--- src/Framework/Input/Mouse.cs
+++ src/Framework/Input/Mouse.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public static class Mouse {
+		public static MouseState GetState( ) { throw new NotImplementedException( ); }
+		public static void SetPosition(int x, int y) { throw new NotImplementedException( ); }
+
+		public static IntPtr WindowHandle { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
 	}
 }
