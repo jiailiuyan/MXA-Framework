a2c0111abaf70fa9fc60ea4f4ab996de6da7a991
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:28:17 +0200

    [Framework] GamePadThumbSticks API was initialized.

--- src/Framework/Input/GamePadThumbSticks.cs
+++ src/Framework/Input/GamePadThumbSticks.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct GamePadThumbSticks {
+		public GamePadThumbSticks(Vector2 leftThumbstick, Vector2 rightThumbstick) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public static bool operator !=(GamePadThumbSticks left, GamePadThumbSticks right) { throw new NotImplementedException( ); }
+		public static bool operator ==(GamePadThumbSticks left, GamePadThumbSticks right) { throw new NotImplementedException( ); }
+
+		public Vector2 Left { get { throw new NotImplementedException( ); } }
+		public Vector2 Right { get { throw new NotImplementedException( ); } }
 	}
 }
