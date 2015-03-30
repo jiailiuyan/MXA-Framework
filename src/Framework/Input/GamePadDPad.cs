2a93e12156c97875e98cb0a8b7d4602bade732a7
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:28:07 +0200

    [Framework] GamePadDPad API was initialized.

--- src/Framework/Input/GamePadDPad.cs
+++ src/Framework/Input/GamePadDPad.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct GamePadDPad {
+		public GamePadDPad(ButtonState upValue, ButtonState downValue, ButtonState leftValue, ButtonState rightValue) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public static bool operator !=(GamePadDPad left, GamePadDPad right) { throw new NotImplementedException( ); }
+		public static bool operator ==(GamePadDPad left, GamePadDPad right) { throw new NotImplementedException( ); }
+
+		public ButtonState Down { get { throw new NotImplementedException( ); } }
+		public ButtonState Left { get { throw new NotImplementedException( ); } }
+		public ButtonState Right { get { throw new NotImplementedException( ); } }
+		public ButtonState Up { get { throw new NotImplementedException( ); } }
 	}
 }
