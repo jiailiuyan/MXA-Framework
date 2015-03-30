e21e1c360a4759e666882073b375b99dad401145
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:28:25 +0200

    [Framework] GamePadTriggers API was initialized.

--- src/Framework/Input/GamePadTriggers.cs
+++ src/Framework/Input/GamePadTriggers.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct GamePadTriggers {
+		public GamePadTriggers(float leftTrigger, float rightTrigger) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public static bool operator !=(GamePadTriggers left, GamePadTriggers right) { throw new NotImplementedException( ); }
+		public static bool operator ==(GamePadTriggers left, GamePadTriggers right) { throw new NotImplementedException( ); }
+
+		public float Left { get { throw new NotImplementedException( ); } }
+		public float Right { get { throw new NotImplementedException( ); } }
 	}
 }
