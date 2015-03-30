85ea93b0dedcc37c22e4e214f4bb9fea8c79f5c8
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:22:58 +0200

    [Framework] AudioEmitter API was initialized.

--- src/Framework/Audio/AudioEmitter.cs
+++ src/Framework/Audio/AudioEmitter.cs
@@ -6,5 +6,12 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Audio {
 	public class AudioEmitter {
+		public AudioEmitter( ) { throw new NotImplementedException( ); }
+
+		public float DopplerScale { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Forward { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Position { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Up { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public Vector3 Velocity { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
 	}
 }
