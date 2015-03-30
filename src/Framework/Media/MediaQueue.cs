fd51cf67673451fe8e3f3d699e9ec580c550405a
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:44:39 +0200

    [Framework] MediaQueue API was initialized.

--- src/Framework/Media/MediaQueue.cs
+++ src/Framework/Media/MediaQueue.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Media {
 	public sealed class MediaQueue {
+		public Song ActiveSong { get { throw new NotImplementedException( ); } }
+		public int ActiveSongIndex { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public int Count { get { throw new NotImplementedException( ); } }
+		public Song this[int index] { get { throw new NotImplementedException( ); } }
 	}
 }
