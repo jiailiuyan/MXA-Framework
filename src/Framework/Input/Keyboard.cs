85a01c4258a39f1c55002e282d4d67d6e85d608e
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:44:19 +0200

    [Framework] Keyboard API was initialized.

--- src/Framework/Input/Keyboard.cs
+++ src/Framework/Input/Keyboard.cs
@@ -6,5 +6,7 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public static class Keyboard {
+		public static KeyboardState GetState( ) { throw new NotImplementedException( ); }
+		public static KeyboardState GetState(PlayerIndex playerIndex) { throw new NotImplementedException( ); }
 	}
 }
