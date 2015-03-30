acdc62cfcd9c421ea3945262e0be1e481010be5a
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:23:16 +0200

    [Framework] DynamicSoundEffectInstance API was initialized.

--- src/Framework/Audio/DynamicSoundEffectInstance.cs
+++ src/Framework/Audio/DynamicSoundEffectInstance.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Audio {
 	public sealed class DynamicSoundEffectInstance : SoundEffectInstance {
+		public DynamicSoundEffectInstance(int sampleRate, AudioChannels channels) { throw new NotImplementedException( ); }
+
+		protected override void Dispose(bool disposing) { throw new NotImplementedException( ); }
+		public TimeSpan GetSampleDuration(int sizeInBytes) { throw new NotImplementedException( ); }
+		public int GetSampleSizeInBytes(TimeSpan duration) { throw new NotImplementedException( ); }
+		public override void Play( ) { throw new NotImplementedException( ); }
+		public void SubmitBuffer(byte[] buffer) { throw new NotImplementedException( ); }
+		public void SubmitBuffer(byte[] buffer, int offset, int count) { throw new NotImplementedException( ); }
+
+
+		public override bool IsLooped { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public int PendingBufferCount { get { throw new NotImplementedException( ); } }
+
+		public event EventHandler<EventArgs> BufferNeeded;
 	}
 }
