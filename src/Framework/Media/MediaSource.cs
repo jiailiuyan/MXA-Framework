421d7a19fd471cd24948f6969ef8ce3cfe28eba3
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:44:58 +0200

    [Framework] MediaSource API was initialized.

--- src/Framework/Media/MediaSource.cs
+++ src/Framework/Media/MediaSource.cs
@@ -6,5 +6,10 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Media {
 	public sealed class MediaSource {
+		public static IList<MediaSource> GetAvailableMediaSources( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public MediaSourceType MediaSourceType { get { throw new NotImplementedException( ); } }
+		public string Name { get { throw new NotImplementedException( ); } }
 	}
 }
