62a2a0e177c8e044460cfe0e38fa0ebc6b17f6f5
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 22:27:38 +0200

    [Framework] GamePadButtons API was initialized.

--- src/Framework/Input/GamePadButtons.cs
+++ src/Framework/Input/GamePadButtons.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct GamePadButtons {
+		public GamePadButtons(Buttons buttons) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public static bool operator !=(GamePadButtons left, GamePadButtons right) { throw new NotImplementedException( ); }
+		public static bool operator ==(GamePadButtons left, GamePadButtons right) { throw new NotImplementedException( ); }
+
+		public ButtonState A { get { throw new NotImplementedException( ); } }
+		public ButtonState B { get { throw new NotImplementedException( ); } }
+		public ButtonState Back { get { throw new NotImplementedException( ); } }
+		public ButtonState BigButton { get { throw new NotImplementedException( ); } }
+		public ButtonState LeftShoulder { get { throw new NotImplementedException( ); } }
+		public ButtonState LeftStick { get { throw new NotImplementedException( ); } }
+		public ButtonState RightShoulder { get { throw new NotImplementedException( ); } }
+		public ButtonState RightStick { get { throw new NotImplementedException( ); } }
+		public ButtonState Start { get { throw new NotImplementedException( ); } }
+		public ButtonState X { get { throw new NotImplementedException( ); } }
+		public ButtonState Y { get { throw new NotImplementedException( ); } }
 	}
 }
