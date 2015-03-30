88ce0e210359e239e560d64905d9f6ca8451e1be
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-30 21:44:10 +0200

    [Framework] GamePad API was initialized.

--- src/Framework/Input/GamePad.cs
+++ src/Framework/Input/GamePad.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public static class GamePad {
+		public static GamePadCapabilities GetCapabilities(PlayerIndex playerIndex) { throw new NotImplementedException( ); }
+		public static GamePadState GetState(PlayerIndex playerIndex) { throw new NotImplementedException( ); }
+		public static GamePadState GetState(PlayerIndex playerIndex, GamePadDeadZone deadZoneMode) { throw new NotImplementedException( ); }
+		public static bool SetVibration(PlayerIndex playerIndex, float leftMotor, float rightMotor) { throw new NotImplementedException( ); }
 	}
 }
