be08774aeae77c43140ec97cbecde9a09017a6f1
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 09:55:15 +0200

    [Framework] KeyboardState API was initialized.

--- src/Framework/Input/KeyboardState.cs
+++ src/Framework/Input/KeyboardState.cs
@@ -6,5 +6,17 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct KeyboardState {
+		public KeyboardState(params Keys[] keys) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public Keys[] GetPressedKeys( ) { throw new NotImplementedException( ); }
+		public bool IsKeyDown(Keys key) { throw new NotImplementedException( ); }
+		public bool IsKeyUp(Keys key) { throw new NotImplementedException( ); }
+
+		public static bool operator !=(KeyboardState a, KeyboardState b) { throw new NotImplementedException( ); }
+		public static bool operator ==(KeyboardState a, KeyboardState b) { throw new NotImplementedException( ); }
+
+		public KeyState this[Keys key] { get { throw new NotImplementedException( ); } }
 	}
 }
