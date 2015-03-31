e95f66af18cf9d48199db1758ca91ba667decd07
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 11:36:32 +0200

    [Framework.Graphics] IGraphicsDeviceService API was initialized.

--- src/Framework.Graphics/IGraphicsDeviceService.cs
+++ src/Framework.Graphics/IGraphicsDeviceService.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public interface IGraphicsDeviceService {
+		GraphicsDevice GraphicsDevice { get; }
+
+		event EventHandler<EventArgs> DeviceCreated;
+		event EventHandler<EventArgs> DeviceDisposing;
+		event EventHandler<EventArgs> DeviceReset;
+		event EventHandler<EventArgs> DeviceResetting;
 	}
 }
