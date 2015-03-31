36799ee289bb1cfaff87233a2e774a7fa62243ab
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 11:42:10 +0200

    [Framework.Graphics] IEffectLights API was initialized.

--- src/Framework.Graphics/IEffectLights.cs
+++ src/Framework.Graphics/IEffectLights.cs
@@ -6,5 +6,12 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public interface IEffectLights {
+		void EnableDefaultLighting( );
+
+		Vector3 AmbientLightColor { get; set; }
+		DirectionalLight DirectionalLight0 { get; }
+		DirectionalLight DirectionalLight1 { get; }
+		DirectionalLight DirectionalLight2 { get; }
+		bool LightingEnabled { get; set; }
 	}
 }
