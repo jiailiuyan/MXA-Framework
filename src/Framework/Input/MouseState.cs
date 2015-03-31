cb3cde55ea08639c67ffdac82b51cb030a05e8de
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 09:55:02 +0200

    [Framework] MouseState API was initialized.

--- src/Framework/Input/MouseState.cs
+++ src/Framework/Input/MouseState.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Input {
 	public struct MouseState {
+		public MouseState(int x, int y, int scrollWheel, ButtonState leftButton, ButtonState middleButton, ButtonState rightButton, ButtonState xButton1, ButtonState xButton2) { throw new NotImplementedException( ); }
+
+		public override bool Equals(object obj) { throw new NotImplementedException( ); }
+		public override int GetHashCode( ) { throw new NotImplementedException( ); }
+		public override string ToString( ) { throw new NotImplementedException( ); }
+
+		public ButtonState LeftButton { get { throw new NotImplementedException( ); } }
+		public ButtonState MiddleButton { get { throw new NotImplementedException( ); } }
+		public ButtonState RightButton { get { throw new NotImplementedException( ); } }
+		public int ScrollWheelValue { get { throw new NotImplementedException( ); } }
+		public int X { get { throw new NotImplementedException( ); } }
+		public ButtonState XButton1 { get { throw new NotImplementedException( ); } }
+		public ButtonState XButton2 { get { throw new NotImplementedException( ); } }
+		public int Y { get { throw new NotImplementedException( ); } }
 	}
 }
