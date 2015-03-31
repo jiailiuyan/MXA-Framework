4bc26284612ac3b1d2405d7782d40d7e5fea8500
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 11:42:00 +0200

    [Framework.Graphics] IEffectFog API was initialized.

--- src/Framework.Graphics/IEffectFog.cs
+++ src/Framework.Graphics/IEffectFog.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public interface IEffectFog {
+		Vector3 FogColor { get; set; }
+		bool FogEnabled { get; set; }
+		float FogEnd { get; set; }
+		float FogStart { get; set; }
 	}
 }
