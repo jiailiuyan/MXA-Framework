5fcb31ef3c454d921997f834aabe053f3dc7341e
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-31 11:53:06 +0200

    [Framework.Graphics] Effect API was initialized.

--- src/Framework.Graphics/Effect.cs
+++ src/Framework.Graphics/Effect.cs
@@ -6,5 +6,15 @@ using System.Threading.Tasks;
 
 namespace Insma.Mxa.Framework.Graphics {
 	public class Effect : GraphicsResource {
+		protected Effect(Effect cloneSource) { throw new NotImplementedException( ); }
+		public Effect(GraphicsDevice graphicsDevice, byte[] effectCode) { throw new NotImplementedException( ); }
+
+		public virtual Effect Clone( ) { throw new NotImplementedException( ); }
+		protected override void Dispose(bool disposing) { throw new NotImplementedException( ); }
+		protected internal virtual void OnApply( ) { throw new NotImplementedException( ); }
+
+		public EffectTechnique CurrentTechnique { get { throw new NotImplementedException( ); } set { throw new NotImplementedException( ); } }
+		public EffectParameterCollection Parameters { get { throw new NotImplementedException( ); } }
+		public EffectTechniqueCollection Techniques { get { throw new NotImplementedException( ); } }
 	}
 }
